99d6c868949152c95af10394f42223e13fcdfebf
Yurii Yaroshenko <yura12323@gmail.com>
2021-07-12 11:35:06 +0300

    [FRONT]: Add some home page styles.

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@
 
           <p class="hero__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, voluptatum!</p>
         </div>
+
+        <div class="hero__img" />
       </div>
     </div>
   </section>
@@ -17,3 +19,33 @@ export default {
   name: 'PageHome',
 }
 </script>
+
+<style lang="scss">
+.hero {
+  position: relative;
+
+  &__inner {
+    display: flex;
+    align-items: center;
+
+    min-height: 100vh;
+  }
+
+  &__text,
+  &__img {
+    width: 50%;
+  }
+
+  &__text {
+    padding-right: 15px;
+  }
+
+  &__img {
+    padding-left: 15px;
+  }
+
+  &__title {
+    margin-bottom: 40px;
+  }
+}
+</style>
