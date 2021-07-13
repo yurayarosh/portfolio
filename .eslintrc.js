b75e5e75971f30adbbf712c55ff3a878aa463dfc
Yurii Yaroshenko <yura12323@gmail.com>
2021-07-13 16:54:12 +0300

    [FRONT]: Add eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,22 @@ module.exports = {
     'vue/multiline-html-element-content-newline': 0,
     'vue/html-closing-bracket-newline': 0,
     'vue/html-indent': 0,
-    'curly': 0,
-    'comma-dangle': ['error', 'always-multiline'],
+    'vue/no-v-html': 0,
+    'vue/comment-directive': 0,
+    'no-template-curly-in-string': 0,
+    curly: 0,
+    camelcase: 0,
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+        },
+      },
+    ],
+    'comma-dangle': ['error', 'only-multiline'],
     'arrow-parens': ['error', 'as-needed'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    indent: ['error', 2, { offsetTernaryExpressions: true }],
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   head: {
     title: 'portfolio',
     meta: [
-      { charset: 'utf-8' },
+      { hid: 'charset', charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
