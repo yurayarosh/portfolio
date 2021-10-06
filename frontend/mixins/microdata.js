271e5e22970f38d31530b067d0b3004dcde75d06
Yurii Yaroshenko <yura12323@gmail.com>
2021-10-06 16:26:11 +0300

    [FRONT]: Prepeare microdata mixin.

---
+++ frontend/mixins/microdata.js
@@ -0,0 +1,71 @@
+export default {
+  data() {
+    return {
+      pageData: {},
+    }
+  },
+  computed: {
+    organizationName() {
+      return this.generalInfo?.organization_name[`title_${this.LANGUAGE}`]
+    },
+  },
+  jsonld() {
+    const baseOrganization = {
+      '@type': 'Organization',
+      name: this.organizationName,
+      url: `${process.env.BASE_URL_PROD}/`,
+      logo: {
+        '@context': 'http://schema.org',
+        '@type': 'ImageObject',
+        url: `${process.env.BASE_URL_PROD}${this.$icon(512)}`,
+        width: 512,
+        height: 512,
+      },
+    }
+
+    const organization = {
+      '@context': 'https://schema.org',
+      ...baseOrganization,
+      sameAs: this.generalInfo?.social.map(({ url }) => url),
+      diversityPolicy: `${process.env.BASE_URL_PROD}/`,
+      ethicsPolicy: `${process.env.BASE_URL_PROD}/`,
+      contactPoint: [
+        {
+          '@type': 'ContactPoint',
+          telephone: this.generalInfo?.phones?.[0][`title_${this.LANGUAGE}`],
+          contactType: 'sales',
+          areaServed: this.LANGUAGE,
+        },
+      ],
+    }
+
+    let breadcrumbsItems = []
+
+    if (this.pageData.breadcrumbs) {
+      breadcrumbsItems = this.pageData.breadcrumbs.map((item, i) => {
+        const id = `${process.env.BASE_URL_PROD}${this.$URL(item.url)}`
+
+        return {
+          '@type': 'ListItem',
+          position: i + 1,
+          item: {
+            id,
+            '@type': 'WebPage',
+            name: item.title,
+          },
+        }
+      })
+    }
+
+    const breadcrumbs =
+      breadcrumbsItems.length > 0
+        ? {
+            '@context': 'https://schema.org',
+            '@type': 'BreadcrumbList',
+            itemListElement: breadcrumbsItems,
+          }
+        : null
+
+    return [organization, breadcrumbs].filter(item => item)
+  },
+}
