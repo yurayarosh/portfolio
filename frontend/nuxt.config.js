69a9140c498df5d34512f19adcab1a94d3b48c0a
yurayarosh <yura12323@gmail.com>
2021-10-14 11:38:06 +0300

    [FRONT]: Add favicon, og:image.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -18,9 +18,54 @@ export default {
   head: {
     title: 'portfolio',
     meta: [
-      { hid: 'charset', charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'theme-color',
+        name: 'theme-color',
+        content: '#fff',
+      },
+      {
+        property: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#0f1012',
+      },
+      {
+        name: 'msapplication-TileImage',
+        content: '/mstile-150x150.png',
+      },
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'mask-icon',
+        color: '#0f1012',
+        href: '/safari-pinned-tab.svg',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
