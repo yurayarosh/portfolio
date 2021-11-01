41e938e88eef6cb6539622370de86f023239ab91
yurayarosh <yura12323@gmail.com>
2021-11-01 12:55:51 +0200

    [FRONT]: enable gtm.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -100,7 +100,7 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/style-resources',
-    // '@nuxtjs/gtm',
+    '@nuxtjs/gtm',
     '@nuxtjs/sitemap',
     [
       'nuxt-font-loader-strategy',
